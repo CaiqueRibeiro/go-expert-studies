a150a67ce5803b3e2d782065f3568136790b54c4
CaiqueRibeiro <ribeiro.caique95@gmail.com>
2024-01-30 00:43:39 -0300

    4.APIS - env configs

---
+++ 4.apis/configs/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"github.com/go-chi/jwtauth"
+	"github.com/spf13/viper"
+)
+
+var cfg *conf
+
+// O viper trabalha com annotations, então é possível mapeá-los para os campos do .env
+type conf struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBHost        string `mapstructure:"DB_HOST"`
+	DBPort        string `mapstructure:"DB_PORT"`
+	DBUser        string `mapstructure:"DB_USER"`
+	DBPassword    string `mapstructure:"DB_PASSWORD"`
+	DBName        string `mapstructure:"DB_NAME"`
+	WebServerPort string `mapstructure:"WEB_SERVER_PORT"`
+	JWTSecret     string `mapstructure:"JWT_SECRET"`
+	JWRExpiresIn  int    `mapstructure:"JWT_EXPIRESIN"`
+	TokenAuth     *jwtauth.JWTAuth
+}
+
+func LoadConfig(path string) (*conf, error) {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv() // Isso permite que, caso existe uma variável de ambiente definida no sistema, sobrescreva a definida no .env
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(err)
+	}
+	// Cria uma instância do TokenAuth para gerar JWT
+	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
+	return cfg, nil
+}
