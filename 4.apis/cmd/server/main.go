321cf7cfe8298168148e3f561f6419e28c03c34e
CaiqueRibeiro <ribeiro.caique95@gmail.com>
2024-02-04 01:43:39 -0300

    product handler (create)

--- 4.apis/cmd/server/main.go
+++ 4.apis/cmd/server/main.go
@@ -1,11 +1,30 @@
 package main
 
-import "github.com/CaiqueRibeiro/4-api/configs"
+import (
+	"net/http"
+
+	"github.com/CaiqueRibeiro/4-api/configs"
+	"github.com/CaiqueRibeiro/4-api/internal/entity"
+	"github.com/CaiqueRibeiro/4-api/internal/infra/database"
+	"github.com/CaiqueRibeiro/4-api/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	config, err := configs.LoadConfig(".")
+	_, err := configs.LoadConfig(".")
 	if err != nil {
 		panic(err)
 	}
-	println(config)
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
 }
