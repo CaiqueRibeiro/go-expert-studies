1355f85edadef5c004d5751f362ae4056f9e9fd6
CaiqueRibeiro <ribeiro.caique95@gmail.com>
2024-02-03 23:09:00 -0300

    chore: add product entity

---
+++ 4.apis/internal/entity/product_test.go
@@ -0,0 +1,45 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/CaiqueRibeiro/4-api/pkg/entity"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProduct(t *testing.T) {
+	t.Run("It should create a new product", func(t *testing.T) {
+		p, err := NewProduct("Product 1", 100)
+		assert.Nil(t, err)
+		assert.Equal(t, "Product 1", p.Name)
+		assert.Equal(t, 100, p.Price)
+		assert.NotEmpty(t, p.ID)
+	})
+
+	t.Run("It should return error when name is empty", func(t *testing.T) {
+		p, err := NewProduct("", 100)
+		assert.Equal(t, ErrNameIsRequired, err)
+		assert.Nil(t, p)
+	})
+
+	t.Run("It should throw error when price is zero", func(t *testing.T) {
+		p, err := NewProduct("Product 1", 0)
+		assert.Equal(t, ErrPriceIsRequired, err)
+		assert.Nil(t, p)
+	})
+
+	t.Run("It should throw error when price is invalid", func(t *testing.T) {
+		p, err := NewProduct("Product 1", -1)
+		assert.Equal(t, ErrPriceIsInvalid, err)
+		assert.Nil(t, p)
+	})
+
+	t.Run("It should validate the product", func(t *testing.T) {
+		p := &Product{
+			ID:    entity.NewID(),
+			Name:  "Product 1",
+			Price: 100,
+		}
+		assert.Nil(t, p.Validate())
+	})
+}
--- 4.apis/internal/entity/user_test.go
+++ 4.apis/internal/entity/user_test.go
@@ -6,19 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewUser(t *testing.T) {
-	user, err := NewUser("Caique", "caique@gmail.com", "123")
-	assert.Nil(t, err)
-	assert.NotNil(t, user)
-	assert.Equal(t, "Caique", user.Name)
-	assert.Equal(t, "caique@gmail.com", user.Email)
-	assert.NotEmpty(t, user.ID)
-	assert.NotEmpty(t, user.Password)
-}
+func TestUser(t *testing.T) {
+	t.Run("it should create a new user", func(t *testing.T) {
+		user, err := NewUser("Caique", "caique@gmail.com", "123")
+		assert.Nil(t, err)
+		assert.NotNil(t, user)
+		assert.Equal(t, "Caique", user.Name)
+		assert.Equal(t, "caique@gmail.com", user.Email)
+		assert.NotEmpty(t, user.ID)
+		assert.NotEmpty(t, user.Password)
+	})
 
-func TestUser_ValidatePassword(t *testing.T) {
-	user, _ := NewUser("Caique", "caique@gmail.com", "123")
-	assert.True(t, user.ValidatePassword("123"))
-	assert.False(t, user.ValidatePassword("1234"))
-	assert.NotEqual(t, "123", user.Password)
+	t.Run("it should validate the password", func(t *testing.T) {
+		user, _ := NewUser("Caique", "caique@gmail.com", "123")
+		assert.True(t, user.ValidatePassword("123"))
+		assert.False(t, user.ValidatePassword("1234"))
+		assert.NotEqual(t, "123", user.Password)
+	})
 }
