502b06aca4ecb209b04fc4a36f877ef29a77190f
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-02-07 23:17:45 -0300

    all products endpoints

---
+++ 5.apis/cmd/server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/CaiqueRibeiro/4-api/configs"
+	"github.com/CaiqueRibeiro/4-api/internal/entity"
+	"github.com/CaiqueRibeiro/4-api/internal/infra/database"
+	"github.com/CaiqueRibeiro/4-api/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products", productHandler.FindAllProducts)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Delete("/products/{id}", productHandler.DeleteProduct)
+
+	http.ListenAndServe(":8000", r)
+}
