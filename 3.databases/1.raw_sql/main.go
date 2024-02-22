aec4359ca4eba8abab4361ace12e01668ca4e8b4
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-02-22 11:28:35 -0300

    start with gorm

--- 3.databases/1.raw_sql/main.go
+++ 3.databases/1.raw_sql/main.go
@@ -1,20 +1,24 @@
 package main
 
-import "github.com/google/uuid"
-import _ "github.com/go-sql-driver/mysql"
-import "database/sql"
-import "fmt"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
-	ID string
-	Name string
+	ID    string
+	Name  string
 	Price float64
 }
 
 func NewProduct(name string, price float64) *Product {
 	return &Product{
-		ID: uuid.New().String(),
-		Name: name,
+		ID:    uuid.New().String(),
+		Name:  name,
 		Price: price,
 	}
 }
@@ -41,18 +45,59 @@ func main() {
 
 func createProduct(db *sql.DB, product *Product) error {
 	stmt, err := db.Prepare("INSERT INTO products(id, name, price) VALUES(?, ?, ?)")
-
 	if err != nil {
 		return err
 	}
-
 	defer stmt.Close()
-
 	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
 
 	if err != nil {
 		return err
 	}
-
 	return nil
+}
+
+func selectProduct(ctx context.Context, db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select id, name, price from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var p Product
+	err = stmt.QueryRowContext(ctx, id).Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
+}
+
+func selectAllProducts(db *sql.DB) ([]*Product, error) {
+	rows, err := db.Query("select id, name, price from products") // não preciso fazer o statement pois não recebo parâmetros
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var products []*Product
+	for rows.Next() {
+		var p Product
+		err = rows.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, &p)
+	}
+	return products, nil
 }
---
+++ 3.databases/2.gorm/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID         int     `gorm:"primaryKey"`
+	Name       string  `json:"name"`
+	Price      float64 `json:"price"`ß
+	gorm.Model         // permite que o GORM adicione campos CreatedAt, UpdatedAt e DeletedAt automaticamente
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{})
+
+	// create one
+	db.Create(&Product{
+		Name:  "Laptop",
+		Price: 3940.99,
+	})
+
+	// create in batch
+	products := []Product{
+		{Name: "Mouse", Price: 9.99},
+		{Name: "Keyboard", Price: 49.99},
+		{Name: "Monitor", Price: 399.99},
+	}
+
+	db.Create(products)
+
+	// soft delete
+	db.Delete(&Product{}) // graças ao gorm.Model, o campo não será deletado, mas sim atualizado o valor de deleted_at
+}
